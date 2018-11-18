a474f656189cd77d338885fe92847b3451ba9f2d
Avi Aryan <avi.aryan123@gmail.com>
2018-11-18 12:54:13 +0530

    move SW registration out of script.js because babel

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const urlsToCache = [
 	'/',
 	'/styles.css',
 	'/script.js',
+	'/register-sw.js',
 	'/img/feather-sprite.svg',
 	'https://unpkg.com/react@16/umd/react.development.js',
 	'https://unpkg.com/react-dom@16/umd/react-dom.development.js',
